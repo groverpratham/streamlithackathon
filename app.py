71481423e7ef21b42c34eb077be1e4cde60455bb
groverpratham <109351933+groverpratham@users.noreply.github.com>
2025-11-11 00:48:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ st.set_page_config(
 )
 
 # Load CSS
-with open("style.css") as f:
+with open("Style.css") as f:
     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
 # Sidebar
@@ -61,3 +61,4 @@ if uploaded_script and uploaded_schema:
     )
 else:
     st.info("Upload both files to start.")
+
